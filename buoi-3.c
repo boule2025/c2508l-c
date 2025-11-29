0ed7db32be23ebd19f10fc68d289957fcd7da508
lê duy vũ <Gm.bgroupviet@gmail.com>
2025-11-29 09:00:30 +0700

    Add files via upload

---
+++ buoi-3.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+
+//int main (){
+//	int a;
+//	int b;
+//	
+//	printf("nhap so a: 4");
+//	scanf("%d", &a);
+//	
+//	printf("nhap so b: 2");
+//	scanf("%d", &b);
+//	
+//	printf("a - b = %d", a - b);
+//	
+//	return 0;
+//
+
+//int main (){
+//	int a;
+//	int b;
+//	
+//	printf("nhap so a: 1");
+//	scanf("%d", &a);
+//	
+//	printf("nhap so b: 2");
+//	scanf("%d", &b);
+//	
+//	printf("a / b = %d", a / b);
+//	
+//	return 0;
+//}
+
+//int main (){
+//	int a;
+//	int b;
+//	
+//	printf("nhap so a: 4");
+//	scanf("%d", &a);
+//	
+//	printf("nhap so b: 2");
+//	scanf("%d", &b);
+//	
+//	printf("a * b = %d", a * b);
+//	
+//	return 0;
+//}
+
+//int main(){
+//	int a, b;
+//	
+//	printf ("nhap so a:");
+//	scanf("%d", &a);
+//	
+//	printf ("Gia tri cua bien a la: %d \n", a);
+//	printf ("Dia chi cua bien a trong RAM la: %p", &a);
+//	
+//	return 0;
+//}
+
+//int main(){
+//	int a = 1, b = 2;
+//	printf ("phep chia lay du cua a va b la: %d", a % b);
+//	
+//	return 0;
+//}
+
+//int main(){
+//	int a = 1, b = 2;
+//	printf ("%d + %d = %d", a, b, a + b);
+//	
+//	return 0;
+//}
+
+//int main(){
+//	int a = 1, b = 2;
+////	printf ("%d + %d = %d", a, b, a + b);
+//	printf ("%d chia lay du cho %d la %d", a, a, a % b);
+//	
+//	return 0;
+//}
+
+//int main(){
+//	int a = 1, b = 2;
+//	printf ("%d chia lay du cho %d la %d", a, b, a % b);
+//	
+//	return 0;
+//}
+
+int main(){
+	int a = 5;
+	
+	printf("gia tri cua bien a la: %d \n", a);
+	a++;
+//	a = a + 1;
+	printf("gia tri cua bien a la: %d", a);
+	
+	return 0;
+}
