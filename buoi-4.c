d4180db88b137f486ea69c4ec07dfb46fa141154
lê duy vũ <Gm.bgroupviet@gmail.com>
2025-12-05 12:13:00 +0700

    Add files via upload

---
+++ buoi-4.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+//	int x = 6;
+//	printf("%d \n", x < 5 && x < 10); // And 
+//	printf("%d \n", x < 5 || x < 10); // Or
+//	printf("%d \n", x < 5 || x < 10); //Not
+
+//	bool isHandsome = false;
+//	printf ("%d", isHandsome);
+
+//	int x = 10;
+//	if (x > 10){
+//		// khoi code nay duoc thuc thi neu dieu kien tren la true
+//		printf ("x > 10");
+//	} else {
+//		printf ("x <= 10");
+//	}
+
+// Viet chuong trinh kiem tra 1 so nguyen nhap vao la so chan hay so le
+
+//int x;
+//printf ("nhap 1 so nguyen bat ky:");
+//scanf ("%d", &x);
+
+//if (x > 0){
+//	if (x % 2 == 0){
+//		printf ("x la so chan");
+//	} else {
+//		printf ("x la so le");
+//	}
+//}else{
+//	printf ("Gia tri nhap vao khong hop le!");
+//}
+
+// bai tap 2: giai phuong trinh bac 1 : ax + b = 0
+// neu a == 0 && b != 0 => chuong trinh vo nghiem
+// neu a == 0 && b == 0 => chuong trinh vo so nghiem
+// x = -b / a;
+// khai bao bien
+//	float a, b, x;
+//    printf("Nhap he so a: ");
+//    scanf("%f", &a);
+//    printf("Nhap he so b: ");
+//    scanf("%f", &b);
+//
+//    if (a == 0) {
+//        if (b == 0) {
+//            printf("Phuong trinh co vo so nghiem.\n");
+//        } else {
+//            printf("Phuong trinh vo nghiem.\n");
+//        }
+//    } else {
+//        x = -b / a;
+//        printf("Phuong trinh co nghiem x = %.2f\n", x);
+//    }
+
+//float a, b, x;
+//printf ("nhap so a: ");
+//scanf ("%f", &a);
+//printf ("nhap so b: ");
+//scanf ("%f", &b);
+//
+//if (a == 0){
+//		if (b != 0){
+//		printf ("Phuong trinh vo nghiem");
+//	} else {
+//		// truong hop b == 0
+//		printf ("Phuong trinh vo so nghiem");
+//	}	
+//	} else {
+//	// truong hop a != 0
+//	x = -b / a;
+//	printf ("nghiem x la: %.2f", x);
+//}
+
+//if (a == 0 && b != 0){
+//	printf ("Phuong trinh vo nghiem");
+//}else if(a == 0 && b == 0){
+//	printf ("Phuong trinh vo so nghiem");
+//}else{
+//	x = -b / a;
+//	printf ("Nghiem x la : %.2f", x);
+//}
+
+//viet chuong trinh nhap vao a,b va in ra so lon nhat (Giai Thuat)
+// cu phap short hand cua if => Toan tu ba ngoi (ternary operator)
+//int a, b, max;
+//printf ("nhap a: ");
+//scanf ("%d", &a);
+//printf ("nhap b: ");
+//scanf ("%d", &b);
+//
+//max = (b > a) ? b : a;
+//printf ("so lon nhat la: %d", max);
+//
+//max = a;
+//if (b > a){
+//	max = b;
+//}
+
+//=> toan tu 2 ngoi
+//a += b; //chinh la a = a + b 
+
+//a++; chinh la a = a + 1; => toan tu 1 ngoi
+
+    return 0;
+}
+
+
+
