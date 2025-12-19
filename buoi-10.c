35390b551784508aac5bd9ce5620bb684f117807
lê duy vũ <Gm.bgroupviet@gmail.com>
2025-12-19 10:58:49 +0700

    Add files via upload

---
+++ buoi-10.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <math.h>
+
+//int main() {
+//	int a;
+//    scanf("%d", &a);
+//    // for (a = 10; a <= 100; a++);
+//    if (a >= 10 && a <= 100){
+//        printf("%d is in range (10, 100)", a);
+//    } else {
+//        printf("%d is not in range (10, 100)", a);
+//    }
+//	return 0;
+//}
+
+//int main(){
+//	int a, b;
+//	scanf ("%d%d", &a, &b);
+//	
+//	if(a >= b){
+//		printf ("a is greater than or equal to b");
+//	} else {
+//		printf("a is smaller than b");
+//	}
+//	return 0;
+//}
+
+
+//int main(){
+//	int a, b;
+//	scanf ("%d%d", &a, &b);
+//	
+//	if (a != 0 && b != 0){
+//		printf("a is not equal to 0 and b is not equal to 0");
+//	} else {
+//		printf ("a is equal to 0 or b is equal to 0");
+//	}	
+//}
+
+
+//int main(){
+//	int a, b, c;
+//	scanf ("%d%d%d", &a, &b, &c);
+//	
+//	if (a >= b && a >= c){
+//		printf ("%d", a);
+//	} else if (b >= c){
+//		printf ("%d", b);
+//	} else {
+//		printf ("%d",c);
+//	}
+//	
+//	return 0;
+//}
